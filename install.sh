d88b826b846c5cfd4e769d4402d6c8afad5facf7
Rustam Gasanov <rustamagasanov@gmail.com>
2015-07-01 16:24:01 +0200

    [^] fixed warning for colors directory

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ git clone git://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 ln -nfs $ROOT_PATH/vimrc $HOME/.vimrc
 vim +BundleInstall +qall
 
-mkdir $HOME/.vim/colors
+mkdir -p $HOME/.vim/colors
 cp $HOME/.vim/bundle/color-schemes/railscasts/base16-railscasts.vim $HOME/.vim/colors/
 
